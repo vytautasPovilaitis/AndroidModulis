dc01e04677d7644bfda7c732947de47b9faddb6f
vytautas.povilaitis <povilaitis.vytas@gmail.com>
2020-04-17 15:54:21 +0300

    Layout faile įdėti du komponentus Button ir TextView; Papildyti Activity failą programiniu kodu taip, kad paspaudus mygtuką, TextView elemente atsirastų pasirinktas užrašas

--- app/src/main/java/com/example/androidmodulis/MainActivity.java
+++ app/src/main/java/com/example/androidmodulis/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.androidmodulis;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final String HelloText = getString(R.string.HelloWorld);
+        Button button1 = (Button) this.findViewById(R.id.button1);
+        final TextView textView = (TextView) findViewById(R.id.textView);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                textView.setText(HelloText);
+            }
+        });
+
+
+
     }
 }

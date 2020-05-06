e49e19a8ba130d43c3171d7fcc9f223d57b20312
vytautas.povilaitis <povilaitis.vytas@gmail.com>
2020-05-06 21:55:25 +0300

    antras modulis ant pirmo...

--- app/src/main/java/com/example/androidmodulis/Functions.java
+++ app/src/main/java/com/example/androidmodulis/Functions.java
@@ -1,4 +1,18 @@
 package com.example.androidmodulis;
 
+import android.content.Context;
+import android.widget.Toast;
+
 public class Functions {
+    public static int countWords(String input) {
+
+        return input.trim().split("\\s+").length;
+
+    }
+
+    public static int countChars(String input) {
+
+        return input.length();
+
+    }
 }
--- app/src/main/java/com/example/androidmodulis/MainActivity.java
+++ app/src/main/java/com/example/androidmodulis/MainActivity.java
@@ -4,9 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import static com.example.androidmodulis.Functions.countChars;
+import static com.example.androidmodulis.Functions.countWords;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,25 +24,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final String HelloText = getString(R.string.HelloWorld);
+        final EditText EditText1 = (EditText) this.findViewById(R.id.editText);
         Button button1 = (Button) this.findViewById(R.id.button1);
-        Button button2 = (Button) this.findViewById(R.id.button2);
         final TextView textView = (TextView) findViewById(R.id.textView);
+        final Spinner spinner = this.findViewById(R.id.spinner);
 
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner_array, android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+
+        final String spinnerType = spinner.getSelectedItem().toString();
         button1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Perform action on click
-                textView.setText(HelloText);
+                String EditText1Value = EditText1.getText().toString();
+                String spinnerValue = spinner.getSelectedItem().toString();
+                int setTextValue;
+
+                if(EditText1Value.isEmpty()) {
+                    Toast.makeText(MainActivity.this, R.string.textFieldEmpty, Toast.LENGTH_LONG).show();
+
+                }else{
+
+                    if(spinnerValue.equals("Words")){
+                        Toast.makeText(MainActivity.this, "Words", Toast.LENGTH_LONG).show();
+                        setTextValue = countWords(EditText1Value);
+
+                    }else{
+                        Toast.makeText(MainActivity.this, "Chars", Toast.LENGTH_LONG).show();
+                        setTextValue = countChars(EditText1Value);
+                    }
+
+                    textView.setText(Integer.toString(setTextValue));
+                }
             }
         });
-
-        button2.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Perform action on click
-                textView.setTextColor(Color.RED);
-            }
-        });
-
-
     }
 }
